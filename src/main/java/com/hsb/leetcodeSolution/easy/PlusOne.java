8a47e73ca049a8e331fc612a7a63ca47ec738dff
heshengbang <yunshixin8521894@sohu.com>
2019-03-02 16:39:11 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcodeSolution/easy/PlusOne.java
@@ -0,0 +1,58 @@
+package com.hsb.leetcodeSolution.easy;
+
+import java.util.Arrays;
+
+/**
+ * Blog: https://www.heshengbang.tech
+ * Twitter: https://twitter.com/heshengbang
+ * Github: https://github.com/heshengbang
+ * Time: 2019/3/1 14:00
+ *
+ * @author heshengbang
+ */
+
+/*
+Plus One
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+
+ */
+
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int one = 1;
+        for (int i = digits.length - 1; i >= 0; i --) {
+            int temp = digits[i] + one;
+            if (temp > 9) {
+                digits[i] = temp % 10;
+            } else {
+                digits[i] = temp;
+                return digits;
+            }
+        }
+        int[] results = new int[digits.length + 1];
+        results[0] = 1;
+        System.arraycopy(digits, 0, results, 1, digits.length);
+        return results;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {1,2,3};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+}
