19b88413134d9464eae16961472b8b547d42adf0
heshengbang <yunshixin8521894@sohu.com>
2019-03-08 09:32:00 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcodeSolution/easy/Same_Tree.java
@@ -0,0 +1,70 @@
+package com.hsb.leetcodeSolution.easy;
+
+/**
+ * Blog: https://www.heshengbang.tech
+ * Twitter: https://twitter.com/heshengbang
+ * Github: https://github.com/heshengbang
+ * Time: 2019/3/8 9:21
+ *
+ * @author heshengbang
+ */
+
+/**
+ * Same Tree
+ * <p>
+ * Given two binary trees, write a function to check if they are the same or not.
+ * <p>
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:     1         1
+ * / \       / \
+ * 2   3     2   3
+ * <p>
+ * [1,2,3],   [1,2,3]
+ * <p>
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input:     1         1
+ * /           \
+ * 2             2
+ * <p>
+ * [1,2],     [1,null,2]
+ * <p>
+ * Output: false
+ * Example 3:
+ * <p>
+ * Input:     1         1
+ * / \       / \
+ * 2   1     1   2
+ * <p>
+ * [1,2,1],   [1,1,2]
+ * <p>
+ * Output: false
+ */
+
+public class Same_Tree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q != null) {
+            return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
