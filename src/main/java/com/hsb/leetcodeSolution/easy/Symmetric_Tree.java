0c72ecbc67636ff9559a852676a10ba55e94aa64
heshengbang <yunshixin8521894@sohu.com>
2019-03-09 17:30:23 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcodeSolution/easy/Symmetric_Tree.java
@@ -0,0 +1,65 @@
+package com.hsb.leetcodeSolution.easy;
+
+/**
+ * Blog: https://www.heshengbang.tech
+ * Twitter: https://twitter.com/heshengbang
+ * Github: https://github.com/heshengbang
+ * Time: 2019/3/8 9:21
+ *
+ * @author heshengbang
+ */
+
+/**
+
+ Symmetric Tree
+
+ Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+ For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+ 1
+ / \
+ 2   2
+ / \ / \
+ 3  4 4  3
+ But the following [1,2,2,null,3,null,3] is not:
+ 1
+ / \
+ 2   2
+ \   \
+ 3    3
+ Note:
+ Bonus points if you could solve it both recursively and iteratively.
+
+ */
+
+public class Symmetric_Tree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return checkSymmetric(root.left, root.right);
+    }
+    public static boolean checkSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && checkSymmetric(left.left, right.right) && checkSymmetric(left.right, right.left);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
