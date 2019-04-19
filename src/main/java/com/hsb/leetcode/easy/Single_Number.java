8fba9643bd28867e60fade63c144bfa919d2a3e3
heshengbang <yunshixin8521894@sohu.com>
2019-04-19 10:42:18 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcode/easy/Single_Number.java
@@ -0,0 +1,45 @@
+package com.hsb.leetcode.easy;
+
+/**
+ * Blog: https://www.heshengbang.tech
+ * Twitter: https://twitter.com/heshengbang
+ * Github: https://github.com/heshengbang
+ * Time: 2019/4/19 9:45
+ *
+ * @author heshengbang
+ */
+
+/**
+ *
+ * Single Number
+ *
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ */
+public class Single_Number {
+    public static int singleNumber(int[] nums) {
+        int value = 0;
+        for (int num: nums) {
+            value = value ^ num;
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        int[] params = {4,1,2,1,2};
+        System.out.println(singleNumber(params));
+    }
+}
