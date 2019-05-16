3c036d6053ab5ff03129bc7cf30482a9fb7a4af3
heshengbang <yunshixin8521894@sohu.com>
2019-05-16 22:23:03 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcode/Medium/Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,37 @@
+package com.hsb.leetcode.Medium;
+
+/**
+ * @author heshengbang
+ * 2019/5/16.
+ * https://github.com/heshengbang
+ * www.heshengbang.men
+ * email: trulyheshengbang@gmail.com
+ */
+public class Longest_Substring_Without_Repeating_Characters {
+    public int lengthOfLongestSubstring(String s) {
+
+        int result = 0;
+        if (s.length() <= 1) {
+            return s.length();
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            String string = s.substring(i);
+            int maxlength = 0;
+            for (int j = 1; j < string.length();j++) {
+                if (string.substring(0, j).indexOf(string.charAt(j)) >= 0) {
+                    maxlength = j;
+                    break;
+                }
+                maxlength = j + 1;
+            }
+            result = (result < maxlength ? maxlength : result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Longest_Substring_Without_Repeating_Characters item = new Longest_Substring_Without_Repeating_Characters();
+        System.out.println(item.lengthOfLongestSubstring("bbbbb"));
+    }
+}
