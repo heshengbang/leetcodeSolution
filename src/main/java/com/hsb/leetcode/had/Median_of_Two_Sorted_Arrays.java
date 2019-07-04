699e0925a44bea0157aa4036fcad70191976eff8
hehu <hu.he@dmall.com>
2019-07-04 16:57:50 +0800

    daily task

--- src/main/java/com/hsb/leetcode/had/Median_of_Two_Sorted_Arrays.java
+++ src/main/java/com/hsb/leetcode/had/Median_of_Two_Sorted_Arrays.java
@@ -18,17 +18,37 @@ package com.hsb.leetcode.had;
  */
 public class Median_of_Two_Sorted_Arrays {
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1[nums1.length - 1] < nums2[0]) {
-            // nums1整体在nums2左边
+        int total = nums1.length + nums2.length;
+        int[] nums = new int[total];
+        int i = 0, j = 0, k = 0;
+        while (k < total) {
+            if (i >= nums1.length) {
+                nums[k] = nums2[j];
+                j++;
+            } else if (j >= nums2.length) {
+                nums[k] = nums1[i];
+                i++;
+            } else if (nums1[i] < nums2[j]) {
+                nums[k] = nums1[i];
+                i++;
+            } else {
+                nums[k] = nums2[j];
+                j++;
+            }
+            k++;
         }
-        if (nums2[nums2.length - 1] < nums1[0]) {
-
+        if (total % 2 == 0) {
+            int middle1 = total / 2;
+            int middle2 = middle1 + 1;
+            return (double)(nums[middle1 - 1] + nums[middle2 - 1]) / 2;
+        } else {
+            int middle = (total + 1) / 2;
+            return nums[middle - 1];
         }
-        return 0;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {-1, -2};
+        int[] nums1 = {-2, -1};
         int[] nums2 = {3};
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
