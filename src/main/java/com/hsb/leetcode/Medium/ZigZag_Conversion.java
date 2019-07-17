8ebb93e9e9e6cbe2fae575c84923edd8e9957ee8
heshengbang <yunshixin8521894@sohu.com>
2019-07-18 00:18:41 +0800

    daily task

--- src/main/java/com/hsb/leetcode/Medium/ZigZag_Conversion.java
+++ src/main/java/com/hsb/leetcode/Medium/ZigZag_Conversion.java
@@ -9,6 +9,7 @@ package com.hsb.leetcode.Medium;
  *************************************************
  */
 
+
 /**
  * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
  *
@@ -36,15 +37,42 @@ package com.hsb.leetcode.Medium;
  * P     I
  */
 public class ZigZag_Conversion {
-    public String convert(String s, int numRows) {
-        for (int row = 0;;row++) {
-            for (int column = 0; column < numRows;column++) {
-                if (row % 3 == 0) {
-
+    public static String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() == 1) {
+            return s;
+        }
+        char[][] arrays = new char[numRows][s.length()];
+        int stringIndex = 0;
+        int mode = numRows - 1;
+        first:
+        for (int column = 0;;column++) {
+            for (int row = 0; row < numRows;row++) {
+                if (stringIndex >= s.length()) {
+                    break first;
+                }
+                if (column % mode == 0) {
+                    arrays[row][column] = s.charAt(stringIndex);
+                    stringIndex++;
                 } else {
-
+                    int rest = column % mode;
+                    arrays[mode - rest][column] = s.charAt(stringIndex);
+                    stringIndex++;
+                    break;
                 }
             }
         }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < numRows;i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (arrays[i][j]!='\u0000') {
+                    stringBuilder.append(arrays[i][j]);
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 4));
     }
 }
