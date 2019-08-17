321f050d8ede99f3c5b867b6223c3a54a87d0b25
heshengbang <yunshixin8521894@sohu.com>
2019-08-17 23:30:51 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcode/easy/Fibonacci_Number.java
@@ -0,0 +1,71 @@
+package com.hsb.leetcode.easy;
+
+/**
+ * @author heshengbang
+ * 2019/8/17.
+ * https://github.com/heshengbang
+ * www.heshengbang.men
+ * email: trulyheshengbang@gmail.com
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * 509. Fibonacci Number
+ *
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+ *
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), for N > 1.
+ * Given N, calculate F(N).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 1
+ * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 2
+ * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+ * Example 3:
+ *
+ * Input: 4
+ * Output: 3
+ * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ *
+ *
+ * Note:
+ *
+ * 0 ≤ N ≤ 30.
+ *
+ */
+public class Fibonacci_Number {
+    private static Map<Integer, Integer> fibMap;
+    public static int fib(int N) {
+        if (fibMap == null) {
+            fibMap = new HashMap<>(N);
+        } else {
+            if (fibMap.containsKey(N)) {
+                return fibMap.get(N);
+            }
+        }
+        if (N < 2) {
+            return N;
+        }
+        int result;
+        result = fib(N - 1) + fib(N - 2);
+        fibMap.put(N, result);
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+}
