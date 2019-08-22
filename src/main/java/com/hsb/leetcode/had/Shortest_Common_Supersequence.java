9dfc0e0d2b56b52d40e12df392daf1af3ccb6f77
heshengbang <yunshixin8521894@sohu.com>
2019-08-22 23:55:51 +0800

    daily task

--- src/main/java/com/hsb/leetcode/had/Shortest_Common_Supersequence.java
+++ src/main/java/com/hsb/leetcode/had/Shortest_Common_Supersequence.java
@@ -11,29 +11,29 @@ package com.hsb.leetcode.had;
 
 /**
  * Shortest Common Supersequence
- *
+ * <p>
  * Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences.  If multiple answers exist, you may return any of them.
- *
+ * <p>
  * (A string S is a subsequence of string T if deleting some number of characters from T (possibly 0, and the characters are chosen anywhere from T) results in the string S.)
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: str1 = "abac", str2 = "cab"
  * Output: "cabac"
  * Explanation:
  * str1 = "abac" is a subsequence of "cabac" because we can delete the first "c".
  * str2 = "cab" is a subsequence of "cabac" because we can delete the last "ac".
  * The answer provided is the shortest such string that satisfies these properties.
- *
- *
+ * <p>
+ * <p>
  * Note:
- *
+ * <p>
  * 1 <= str1.length, str2.length <= 1000
  * str1 and str2 consist of lowercase English letters.
- *
- *
+ * <p>
+ * <p>
  * "bbbaaaba"  "bbababbb"   "bbabaaababb"
  */
 public class Shortest_Common_Supersequence {
@@ -41,27 +41,28 @@ public class Shortest_Common_Supersequence {
         if (str1.length() == 0 || str2.length() == 0 || str1.contains(str2) || str2.contains(str1)) {
             return str1.length() < str2.length() ? str2 : str1;
         }
-        // str1 + str2
-        String result1 = "";
-        for (int i = 0; i < str1.length(); i++) {
-            String subString = str1.substring(i);
-            if (str2.startsWith(subString)) {
-                result1 = str1.substring(0, i) + str2;
+        String[][] results = new String[str1.length() + 1][str2.length() + 1];
+        for (int str1Index = 1; str1Index < str1.length() + 1; str1Index++) {
+            for (int str2Index = 1; str2Index < str2.length() + 1; str2Index++) {
+                if (str1.charAt(str1Index - 1) == str2.charAt(str2Index - 1)) {
+                    if (results[str1Index - 1][str2Index - 1] == null) {
+                        results[str1Index - 1][str2Index - 1] = "";
+                    }
+                    results[str1Index][str2Index] = results[str1Index - 1][str2Index - 1] + str1.charAt(str1Index - 1);
+                } else {
+                    if (results[str1Index - 1][str2Index] == null) {
+                        results[str1Index - 1][str2Index] = "";
+                    }
+                    String temp1 = results[str1Index - 1][str2Index];
+                    if (results[str1Index][str2Index - 1] == null) {
+                        results[str1Index][str2Index - 1] = "";
+                    }
+                    String temp2 = results[str1Index][str2Index - 1];
+                    results[str1Index][str2Index] = temp1.length() < temp2.length() ? temp1 : temp2;
+                }
             }
         }
-        // str2 + str1
-        String result2 = "";
-        for (int i = 0; i < str2.length(); i++) {
-            String subString = str2.substring(i);
-            if (str1.startsWith(subString)) {
-                result2 = str2.substring(0, i) + str1;
-            }
-        }
-        if (result1.length() != 0 && result2.length() != 0) {
-            return result1.length() < result2.length() ? result1 : result2;
-        } else {
-            return result1.length() < result2.length() ? result2 : result1;
-        }
+        return results[str1.length()][str2.length()];
     }
 
     public static void main(String[] args) {
