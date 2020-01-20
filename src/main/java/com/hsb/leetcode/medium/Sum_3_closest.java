1208d80a5103973c9fc2be6216f4ed851afad7a8
hehu <hu.he@dmall.com>
2020-01-20 23:34:55 +0800

    daily task

---
+++ src/main/java/com/hsb/leetcode/medium/Sum_3_closest.java
@@ -0,0 +1,57 @@
+package com.hsb.leetcode.medium;
+
+/*
+ * @since 2020/1/20 23:03
+ * @version 1.0
+ */
+
+import java.util.Arrays;
+
+public class Sum_3_closest {
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return nums[0] + nums[1];
+        } else if (nums.length == 3) {
+            return nums[0] + nums[1] + nums[2];
+        }
+        Arrays.sort(nums);
+        int closest = Integer.MAX_VALUE;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int start = 0, end = nums.length - 1;
+            while (start < end) {
+                if (start == i) {
+                    start++;
+                    continue;
+                } else if (end == i) {
+                    end--;
+                    continue;
+                }
+                int total = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - total) < closest) {
+                    closest = Math.abs(target - total);
+                    result = total;
+                }
+                if (total > target) {
+                    end--;
+                } else if (total < target){
+                    start++;
+                } else {
+                    return target;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Sum_3_closest item = new Sum_3_closest();
+        int[] nums = {1,1,1,1};
+        int target = 3;
+        System.out.println(item.threeSumClosest(nums, target));
+    }
+}
