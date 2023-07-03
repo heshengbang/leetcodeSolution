97538eb311130652ce98b4c4aeec734e625d420a
heshengbang <trulyheshengbang@gmail.com>
2023-07-03 18:06:24 +0800

    add new solution

--- src/main/java/com/hsb/leetcode/easy/Merge_Sorted_Array.java
+++ src/main/java/com/hsb/leetcode/easy/Merge_Sorted_Array.java
@@ -49,6 +49,56 @@ public class Merge_Sorted_Array {
     public static void main(String[] args) {
         int[] nums1 = {1};
         int[] nums2 = {};
-        System.out.println(Arrays.toString(merge(nums1, 1, nums2, 0)));
+        System.out.println(Arrays.toString(merge1(nums1, 1, nums2, 0)));
+
+
+//        int[] nums1 = {1,2,3,0,0,0};
+//        int m = 3;
+//        int[] nums2 = {2,5,6};
+//        int n = 3;
+//        System.out.println(Arrays.toString(merge1(nums1, m, nums2, n)));
+
+
+//        int[] nums1 = {0};
+//        int m = 0;
+//        int[] nums2 = {1};
+//        int n = 1;
+//        System.out.println(Arrays.toString(merge1(nums1, m, nums2, n)));
+
+//        int[] nums1 = {2, 0};
+//        int m = 1;
+//        int[] nums2 = {1};
+//        int n = 1;
+//        System.out.println(Arrays.toString(merge1(nums1, m, nums2, n)));
+    }
+
+
+    public static int[] merge1(int[] nums1, int m, int[] nums2, int n) {
+        if (m == 0) {
+            System.arraycopy(nums2, 0, nums1, 0, n);
+        } else if (n == 0) {
+            // do nothing
+        } else {
+            int j = n - 1;
+            int count = m + n -1;
+            for (int i = m - 1;i >= 0 && j >= 0 && count >= 0;) {
+                if (nums1[i] > nums2[j]) {
+                    nums1[count] = nums1[i];
+                    i--;
+                    if (i < 0) {
+                        System.arraycopy(nums2, 0, nums1, 0, count);
+                        break;
+                    }
+                } else {
+                    nums1[count] = nums2[j];
+                    j--;
+                    if (j < 0) {
+                        break;
+                    }
+                }
+                count--;
+            }
+        }
+        return nums1;
     }
 }
