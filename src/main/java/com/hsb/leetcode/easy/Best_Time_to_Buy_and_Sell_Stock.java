27da383e4460e3c1c2103dd2e3d93a4f8ce68199
heshengbang <trulyheshengbang@gmail.com>
2023-07-05 22:44:52 +0800

    add new solution

--- src/main/java/com/hsb/leetcode/easy/Best_Time_to_Buy_and_Sell_Stock.java
+++ src/main/java/com/hsb/leetcode/easy/Best_Time_to_Buy_and_Sell_Stock.java
@@ -10,45 +10,82 @@ package com.hsb.leetcode.easy;
  */
 
 /**
- *
  * Best Time to Buy and Sell Stock
- *
+ * <p>
  * Say you have an array for which the ith element is the price of a given stock on day i.
- *
+ * <p>
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
- *
+ * <p>
  * Note that you cannot sell a stock before you buy one.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: [7,1,5,3,6,4]
  * Output: 5
  * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
- *              Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
  * Example 2:
- *
+ * <p>
  * Input: [7,6,4,3,1]
  * Output: 0
  * Explanation: In this case, no transaction is done, i.e. max profit = 0.
- *
  */
 public class Best_Time_to_Buy_and_Sell_Stock {
-    public static int maxProfit(int[] prices) {
+    public static int maxProfit1(int[] prices) {
         int profit = 0;
         for (int i = 0; i < prices.length; i++) {
             for (int j = i; j < prices.length; j++) {
+                if (prices[i] > prices[i]) {
+                    continue;
+                }
                 profit = Math.max(profit, prices[j] - prices[i]);
             }
         }
-        if (profit <= 0) {
+        return profit <= 0 ? 0 : profit;
+    }
+
+    public int maxProfit2(int[] prices) {
+        if (prices.length < 2) {
             return 0;
-        } else {
-            return profit;
         }
+        int goodBuyPrice = Integer.MAX_VALUE;
+        int profit = 0, preProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (goodBuyPrice < prices[i]) {
+                profit = Math.max(profit, prices[i] - goodBuyPrice);
+            } else if (goodBuyPrice > prices[i]) {
+                preProfit = Math.max(profit, preProfit);
+                goodBuyPrice = prices[i];
+                profit = 0;
+            }
+        }
+        return Math.max(profit, preProfit);
+    }
+
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        int buy = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (buy < prices[i]) {
+                int profit = prices[i] - buy;
+                maxprofit = Math.max(maxprofit, profit);
+            } else
+                buy = prices[i];
+        }
+        return maxprofit;
     }
 
     public static void main(String[] args) {
-        int[] array = {2,4,1};
-        System.out.println(maxProfit(array));
+        Best_Time_to_Buy_and_Sell_Stock it = new Best_Time_to_Buy_and_Sell_Stock();
+//        int[] array = {2,4,1};
+//        System.out.println(it.maxProfit2(array));
+
+//        int[] array = {7,1,5,3,6,4};
+//        System.out.println(it.maxProfit2(array));
+
+        int[] array = {3, 9, 2, 8, 1, 5};
+        System.out.println(it.maxProfit3(array));
     }
 }
--- src/main/java/com/hsb/leetcode/medium/Rotate_Array.java
+++ src/main/java/com/hsb/leetcode/medium/Rotate_Array.java
@@ -3,7 +3,7 @@ package com.hsb.leetcode.medium;
 import java.util.Arrays;
 
 public class Rotate_Array {
-    public static void rotate(int[] nums, int k) {
+    public static void rotate1(int[] nums, int k) {
         if (nums.length == 1 || k == 0) {
             return;
         }
@@ -24,9 +24,62 @@ public class Rotate_Array {
         }
     }
 
+    /**
+     * solution with bad space usage
+     * @param nums
+     * @param k
+     */
+    public static void rotate2(int[] nums, int k) {
+        if (k > nums.length) {
+            k = k % nums.length;
+        }
+        if (nums.length < 2 || k < 1) {
+            return;
+        }
+        int[] tmp = new int[k];
+        System.arraycopy(nums, nums.length - k, tmp, 0, k);
+        System.arraycopy(nums, 0, nums, k, nums.length - k);
+        System.arraycopy(tmp, 0, nums, 0, k);
+    }
+
+
+    public static void rotate3(int[] nums, int k) {
+        if (k > nums.length) {
+            k = k % nums.length;
+        }
+        if (nums.length < 2 || k == 0) {
+            return;
+        }
+        reverseArray(nums, 0, nums.length - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, nums.length - 1);
+    }
+
+    private static void reverseArray(int[] nums, int start, int end) {
+        int tmp;
+        for (int i = start, j = end; i < j; i++, j--) {
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+
     public static void main(String[] args) {
+//        int[] param = {1, 2};
+//        rotate2(param, 3);
+//        System.out.println(Arrays.toString(param));
+
+
+//        int[] param = {1,2,3,4,5,6,7};
+//        rotate3(param, 3);
+//        System.out.println(Arrays.toString(param));
+
+//        int[] param = {-1,-100,3,99};
+//        rotate3(param, 2);
+//        System.out.println(Arrays.toString(param));
+
         int[] param = {1, 2};
-        rotate(param, 3);
+        rotate3(param, 3);
         System.out.println(Arrays.toString(param));
     }
 }
